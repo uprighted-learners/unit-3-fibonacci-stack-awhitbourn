31faf47c381d66bcf5e3159d51a5f3f0fc1a41fb
awhitbourn <aprilwhitbourn@gmail.com>
2024-08-06 23:54:27 -0400

    complete

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 // Create the `series` variable
 var series = [0, 1];
 
-while (/* your check here */) {
-  // your code here
+while (series.length < 10) {
+  // Pop the last two numbers from the array
+  let num1 = series.pop();
+  let num2 = series.pop();
+
+  // Calculate the sum of these two numbers
+  let sum = num1 + num2;
+
+  //Push the two numbers back onto the array
+  series.push(num2);
+  series.push(num1);
+
+  //Push the sum onto the array
+  series.push(sum);
 }
 
 // Print the Fibonacci sequence to the console
